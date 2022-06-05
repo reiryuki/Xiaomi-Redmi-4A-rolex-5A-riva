b93bbab5583776c55a54ef115157bb3ba2369b23
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2022-06-05 22:53:55 +0700

    Create utils.c

---
+++ power/utils.c
@@ -0,0 +1,6 @@
+# msm8937 4 CPU Cores & msm8917
+# This line have to be no more than 4 CPU cores
+const char* scaling_gov_path[4] = {"/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor",
+                                   "/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor",
+                                   "/sys/devices/system/cpu/cpu2/cpufreq/scaling_governor",
+                                   "/sys/devices/system/cpu/cpu3/cpufreq/scaling_governor"};
