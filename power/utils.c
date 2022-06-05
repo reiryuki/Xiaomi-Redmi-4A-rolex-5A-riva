9596dfc903ff05e99c8bb25c0e7e749aa83174fa
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2022-06-05 22:57:59 +0700

    Update utils.c

--- power/utils.c
+++ power/utils.c
@@ -1,4 +1,5 @@
 # msm8937 4 CPU Cores & msm8917
+# Building /vendor/bin/hw/android.hardware.power-service-qti
 # This line have to be no more than 4 CPU cores
 const char* scaling_gov_path[4] = {"/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor",
                                    "/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor",
